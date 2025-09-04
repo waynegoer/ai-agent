bacccec1087264aef6d814444e2bf3d355cd38b8
waynegoer <waynegoer@gmail.com>
2025-09-04 23:36:12 +0900

    MCP

---
+++ image-search-mcp-server/src/main/java/com/example/imagesearchmcpserver/tools/ImageSearchTool.java
@@ -0,0 +1,61 @@
+package com.example.imagesearchmcpserver.tools;
+
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class ImageSearchTool {
+
+    //  Pexels API
+    private static final String API_KEY = "eW3TN3xEBSjAxGNa3uuW0jgHtBWhNlm1dw7xhErLFC5mqNnTCQSoMjyd";
+
+    // Pexels APIURL
+    private static final String API_URL = "https://api.pexels.com/v1/search";
+
+    @Tool(description = "search image from web")
+    public String searchImage(@ToolParam(description = "Search query keyword") String query) {
+        try {
+            return String.join(",", searchMediumImages(query));
+        } catch (Exception e) {
+            return "Error search image: " + e.getMessage();
+        }
+    }
+
+    /**
+     * 搜索中等尺寸的图片列表
+     *
+     * @param query
+     * @return
+     */
+    public List<String> searchMediumImages(String query) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Authorization", API_KEY);
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("query", query);
+
+        String response = HttpUtil.createGet(API_URL)
+                .addHeaders(headers)
+                .form(params)
+                .execute()
+                .body();
+        return JSONUtil.parseObj(response)
+                .getJSONArray("photos")
+                .stream()
+                .map(photoObj -> (JSONObject) photoObj)
+                .map(photoObj -> photoObj.getJSONObject("src"))
+                .map(photo -> photo.getStr("medium"))
+                .filter(StrUtil::isNotBlank)
+                .collect(Collectors.toList());
+    }
+}
